6f27f6c7d66d2b9747e13a0ecb3c730241af7fb7
Viacheslav Voytovich <voytovich.slava@gmail.com>
2021-04-22 22:38:42 +0700

    install react and make the structure of folders

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import '../stylesheets';
+
+import App from './components/app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
